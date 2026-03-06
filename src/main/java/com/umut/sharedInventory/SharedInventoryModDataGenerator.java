88a03e3cdf567462a6a102dac0c9c92aab48e9ce
C2C867 <u_petrichor@bupt.edu.cn>
2026-03-06 11:10:40 +0800

    feat: sync source code for 1.19.3

---
+++ src/main/java/com/umut/sharedInventory/SharedInventoryModDataGenerator.java
@@ -0,0 +1,11 @@
+package com.umut.sharedInventory;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class SharedInventoryModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
---
+++ src/main/java/com/umut/sharedInventory/mixin/SharedInventoryPlayerEntityMixin.java
@@ -0,0 +1,45 @@
+package com.umut.sharedInventory.mixin;
+
+import com.umut.sharedInventory.objects.SharedInventoryPlayerEntity;
+import com.umut.sharedInventory.objects.SharedInventoryPlayerPrivateInventory;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.nbt.NbtCompound;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin (PlayerEntity.class)
+public class SharedInventoryPlayerEntityMixin implements SharedInventoryPlayerEntity {
+    @Unique
+    protected SharedInventoryPlayerPrivateInventory sharedInventory$playerPrivateInventory = new SharedInventoryPlayerPrivateInventory();
+
+
+
+    @Inject(
+            method = "writeCustomDataToNbt",
+            at = @At("TAIL")
+    )
+    private void writePrivateInventory(NbtCompound nbt, CallbackInfo ci) {
+        nbt.put("sharedInventoryPrivateInventory", this.sharedInventory$playerPrivateInventory.toNbtList());
+    }
+
+    @Inject(method = "readCustomDataFromNbt", at = @At("TAIL"))
+    private void readPrivateInventory(NbtCompound nbt, CallbackInfo ci) {
+        if (nbt.contains("sharedInventoryPrivateInventory", 9)) {
+            this.sharedInventory$playerPrivateInventory.readNbtList(nbt.getList("sharedInventoryPrivateInventory", 10));
+        }
+    }
+
+    @Override
+    public SharedInventoryPlayerPrivateInventory shared_inventory1_18_2$getPrivateInventory() {
+        return this.sharedInventory$playerPrivateInventory;
+    }
+
+    @Override
+    public void shared_inventory1_18_2$setPrivateInventory(SharedInventoryPlayerPrivateInventory privateInventory){
+        this.sharedInventory$playerPrivateInventory=privateInventory;
+    }
+
+}
